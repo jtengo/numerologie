5f2d28bd2b7a59890f533cebcbc0f0ce1afe1e84
jtengo <jose.tengo@centrale-marseille.fr>
2022-10-15 23:49:55 +0200

    rend un chiffre à partir d'une chaîne de caractères

--- numerologie.js
+++ numerologie.js
@@ -6,6 +6,24 @@ function nombre(chaine) {
     return somme
 }
 
+function somme(nombre) {
+    var somme = 0
+    chaine = String(nombre)
+    for (var i=0; i < chaine.length ; i++) {
+        somme += parseInt(chaine.charAt(i))
+    }
+    return somme
+}
+
+function chiffreAssocie(chaine) {
+    valeur = nombre(chaine)
+
+    while (valeur > 9) {
+        valeur = somme(valeur)
+    }
+    return valeur
+}
+
 // test de nombre(chaine)
 
 // est-ce 2x plus ?
@@ -16,4 +34,22 @@ console.log(nombre("coucou"))
 for (c of "cou") { 
     console.log(c + " : " + nombre(c))
 }
-// fin de test de nombre(chaine)
+// test de somme(nombre)
+console.log(somme(132))
+
+// avec un chiffre : charAt != charCodeAt
+console.log(somme(4))
+console.log("4".charCodeAt(0))
+console.log("4".charAt(0))
+
+// conversion chaine de caractères et nombre
+console.log(typeof "4".charAt(0))
+console.log(parseInt("4".charAt(0)))
+console.log(typeof parseInt("4".charAt(0)))
+
+// test de chiffreAssocie(chaine)
+
+//test valeur somme des chiffres
+console.log(nombre("coucou"))
+console.log(chiffreAssocie("coucou"))
+// fin de test de chiffreAssocie(chaine)
